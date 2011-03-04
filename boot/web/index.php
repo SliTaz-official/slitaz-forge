d5a77302e5cced6e8c04797d8c5a286e7a42b29c
Christopher Rogers <slaxemulator@gmail.com>
2011-03-04 11:27:53 +0000

    Add boot/web folder. This is so we have a backup of boot.slitaz.org website.

---
+++ boot/web/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>SliTaz Web boot</title>
+    <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+    <meta name="description" content="slitaz gPXE boot online your OS web-boot slitaz-cooking" />
+    <meta name="keywords" lang="en" content="slitaz, boot, pxe, web OS" />
+    <meta name="robots" content="index, follow, all" />
+    <meta name="revisit-after" content="7 days" />
+    <meta name="expires" content="never" />
+    <meta name="modified" content="<?php echo (date( "Y-m-d H:i:s", getlastmod())); ?>" />
+    <meta name="author" content="Christophe Lincoln"/>
+    <meta name="publisher" content="www.slitaz.org" />
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="slitaz.css" />
+    <link rel="Content" href="#content" />
+</head>
+<body>
+
+<!-- Header -->
+<div id="header">
+    <a href="http://boot.slitaz.org/"><img id="logo"
+		src="pics/website/logo.png" 
+		title="boot.slitaz.org" alt="boot.slitaz.org" /></a>
+    <p id="titre">#!/Web/Boot</p>
+</div>
+
+<!-- Content -->
+<div id="content-full">
+
+<!-- Block begin -->
+<div class="block">
+	<!-- Nav block begin -->
+	<div id="block_nav">
+		<h3><img src="pics/website/users.png" alt="users.png" />Community</h3>
+		<ul>
+			<li><a href="http://scn.slitaz.org/">Community Network</a></li>
+			<li><a href="http://pizza.slitaz.org/">LiveCD Online Builder</a></li>
+		</ul>
+		<h3>Quick Download</h3>
+		<div class="button">
+			<a href="http://mirror.slitaz.org/boot/slitaz-boot.iso">slitaz-boot.iso</a>
+		</div>
+	<!-- Nav block end -->
+	</div>
+	<!-- Top block begin -->
+	<div id="block_top">
+		<h1>SliTaz Web boot</h1>
+		<p>
+			Welcome to the <a href="http://www.slitaz.org/en/">SliTaz GNU/Linux</a>
+			gPXE boot host; boot.slitaz.org allows you to boot SliTaz from the Web
+			using a cdrom, USB media or a floppy disk. The Linux Kernel and the 
+			complete SliTaz compressed root filesystem will be loaded into RAM 
+			from the Web using PXE and HTTP protocols. 
+			<a href="guide.html">Short guide...</a>
+		</p>
+	<!-- Top block end -->
+	</div>
+<!-- Block end -->
+</div>
+
+<h2>In your language</h2>
+
+<h3>Français</h3>
+<p>
+Bienvenue sur l'hôte de démarrage gPXE de 
+<a href="http://www.slitaz.org/">SliTaz GNU/Linux</a>, boot.slitaz.org
+vous permet de démarrer SliTaz depuis le réseau en utilisant un cdrom,
+une clé USB ou une disquette de démarrage. Le noyau Linux et le système
+de fichiers compressé contenant SliTaz seront chargés en RAM depuis
+internet en utilisant les protocoles PXE et HTTP. 
+<a href="guide.html#fr">Guide rapide...</a>
+</p>
+
+<h3>Deutsch</h3>
+<p>
+Willkommen am Start-Provider gPXE von
+<a href="http://www.slitaz.org/">SliTaz GNU/Linux</a>,
+boot.slitaz.org ermöglicht Ihnen, SliTaz aus dem Netzwerk durch 
+einfache benützung einer CD-ROM, eines USB-Sticks oder gar einer
+Start-Floppy, einzusetzen. Linux-Kernel und erforderliche komprimierte
+Dateien werden dann in Ihr RAM aus dem Internet über das Protokol 
+PXE/HTTP geladen. Viel Freude damit! <a href="guide.html#en">Short guide...</a>
+</p>
+
+<!-- End of content -->
+</div>
+
+<!-- Footer -->
+<div id="footer">
+	<div class="right_box">
+	<h4>SliTaz Network</h4>
+		<ul>
+			<li><a href="http://doc.slitaz.org/">Documentation</a></li>
+			<li><a href="http://forum.slitaz.org/">Support Forum</a></li>
+			<li><a href="http://scn.slitaz.org/">Community Network</a></li>
+			<li><a href="http://labs.slitaz.org/">Laboratories</a></li>
+			<li><a href="http://twitter.com/slitaz">SliTaz on Twitter</a></li>
+			<li><a href="http://distrowatch.com/slitaz">SliTaz on DistroWatch</a></li>
+		</ul>
+	</div>
+	<h4>SliTaz Website</h4>
+	<ul>
+		<li><a href="#header">Top of the page</a></li>
+		<li>Copyright &copy; <span class="year"></span>
+			<a href="http://www.slitaz.org/">SliTaz</a></li>
+		<li><a href="http://www.slitaz.org/en/about/">About the project</a></li>
+		<li><a href="http://www.slitaz.org/netmap.php">Network Map</a></li>
+		<li>Page modified the <?php echo (date( "d M Y", getlastmod())); ?></li>
+		<li><a href="http://validator.w3.org/check?uri=referer"><img
+		src="pics/website/xhtml10.png" alt="Valid XHTML 1.0"
+		title="Code validé XHTML 1.0"
+		style="width: 80px; height: 15px; vertical-align: middle;" /></a></li>
+	</ul>
+</div>
+
+</body>
+</html>
