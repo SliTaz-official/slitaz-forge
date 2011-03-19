f495df54575c867297d5fb722d9a89e5117b15a0
Christophe Lincoln <pankso@slitaz.org>
2011-03-19 16:26:12 +0100

    Update laste forum theme

--- forum/vanilla/themes/slitaz/about.php
+++ forum/vanilla/themes/slitaz/about.php
@@ -1,11 +1,6 @@
 <?php if (!defined('APPLICATION')) exit();
 /*
-Copyright 2008, 2009 Vanilla Forums Inc.
-This file is part of Garden.
-Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
-Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
+	Copyright 2011 SliTaz GNU/Linux.
 */
 
 /**
@@ -13,9 +8,9 @@ Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
  */
 $ThemeInfo['SliTaz'] = array(
    'Name' => 'SliTaz',
-   'Description' => "SliTaz Vanilla 2 official theme",
-   'Version' => '1.0',
+   'Description' => "SliTaz official theme for Vanilla 2 ",
+   'Version' => '4.0',
    'Author' => "Christophe Lincoln",
    'AuthorEmail' => 'pankso@slitaz.org',
-   'AuthorUrl' => 'http://www.slitaz.org'
+   'AuthorUrl' => 'http://www.slitaz.org/'
 );
