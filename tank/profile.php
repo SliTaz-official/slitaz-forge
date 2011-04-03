b5f3b8d93ea558d4ce0acc3b26b03e5d219ce733
Christopher Rogers <slaxemulator@gmail.com>
2011-04-03 04:11:27 +0000

    taztank: Add profile.php as a file into /usr/share/taztank folder. The current method will not work since dollar sign versions in php are blank.

---
+++ tank/profile.php
@@ -0,0 +1,13 @@
+<?php
+
+// Your custom SliTaz profile at http://people.slitaz.org/. This file must
+// be located at ~/Public/profile.php. You must at least provide your real
+// name and you can use the $wall variable for a personnal message.
+
+$name = "Real Name";
+$location = "";
+$scn_user = "";
+$skills = "";
+$wall = "";
+
+?>
