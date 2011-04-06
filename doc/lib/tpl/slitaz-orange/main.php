655da6283654bb307f8ba1341998f705f2db281a
Christophe Lincoln <pankso@slitaz.org>
2011-04-06 02:29:19 +0200

    Add latest Dokuwiki theme

---
+++ doc/lib/tpl/slitaz-orange/main.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * DokuWiki SliTaz Template - With code from the default theme by
+ * Andreas Gohr <andi@splitbrain.org>
+ * 
+ */
+
+// must be run from within DokuWiki
+if (!defined('DOKU_INC')) die();
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>"
+ lang="<?php echo $conf['lang']?>" dir="<?php echo $lang['direction']?>">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>
+    <?php tpl_pagetitle()?>
+    [<?php echo strip_tags($conf['title'])?>]
+  </title>
+  <?php tpl_metaheaders()?>
+  <link rel="shortcut icon" href="<?php echo DOKU_TPL?>images/favicon.ico" />
+</head>
+<body>
+
+<div class="dokuwiki">
+
+<!-- Header -->
+<div id="header">
+	<div id="logo"></div>
+	<!-- SliTaz Network -->
+	<div id="network">
+		<a href="http://www.slitaz.org/">
+			<img src="<?php echo DOKU_TPL?>images/network.png" alt="network.png" /></a>
+		<a href="http://scn.slitaz.org/">Community</a>
+		<a href="http://doc.slitaz.org/">Doc</a>
+		<a href="http://forum.slitaz.org/">Forum</a>
+		<a href="http://labs.slitaz.org/issues">Bugs</a>
+		<a href="http://hg.slitaz.org/">Hg</a>
+	</div>
+	<h1><a href="./">SliTaz Doc</a></h1>
+	<!-- <?php tpl_link(wl($ID,'do=backlink'),tpl_pagetitle($ID,true),'title="'.$lang['btn_backlink'].'"')?> -->
+</div>
+
+<!-- SliTaz Block -->
+<div id="block">
+	<?php html_msgarea()?>
+	<!-- Languages -->
+	<div id="lang">
+	<?php
+		$translation = &plugin_load('helper','translation');
+		echo $translation->showTranslations();
+	?>
+	</div>
+	<div style="text-align: justify; width: 48%; padding: 0 10px 0 0;">
+		The SliTaz Community Network let you connect with SliTaz
+		users and developpers, share eveything about SliTaz,
+		help others and get involve in the project.
+	</div>
+	<div class="tools">
+		<img src="<?php echo DOKU_TPL?>images/tools.png" alt=".png" />
+		
+		<div style="float: right; text-align: right;">
+			<?php tpl_button('edit')?>
+			<?php tpl_searchform()?>
+		</div>
+		<?php tpl_button('recent')?>
+		<?php tpl_button('subscription')?>
+		<?php tpl_button('admin')?>
+		<?php tpl_button('profile')?>
+		<?php tpl_button('login')?>
+	</div>
+</div>
+
+<!-- Content -->
+<div id="content">
+
+<?php flush()?>
+
+<!-- wikipage start -->
+<?php tpl_content()?>
+<!-- wikipage stop -->
+
+<div class="clearer">&nbsp;</div>
+<?php flush()?>
+
+<div class="meta">
+  <div class="user">
+	<?php tpl_userinfo()?>
+  </div>
+  <div class="doc">
+	<?php tpl_pageinfo()?>
+  </div>
+</div>
+
+<div style="border-top: 1px solid #eaeaea;">
+	<div style="float: right; text-align: right;">
+		<?php tpl_button('edit')?>
+		<?php tpl_button('history')?>
+	</div>
+	<?php tpl_button('recent')?>
+	<?php tpl_button('index')?>
+</div>
+
+<!-- End of content -->
+</div>
+
+<!-- End of: class="dokuwiki" -->
+</div>
+
+<!-- Footer -->
+<div id="footer">
+	Copyright &copy; <?php echo date('Y'); ?>
+	<a href="http://www.slitaz.org/">SliTaz</a> 
+	<a href="http://www.gnu.org/copyleft/fdl.html">GNU FDL</a>
+	- <a href="/start?do=login">Login Wiki</a>
+	<p>
+		SliTaz @
+		<a href="http://twitter.com/slitaz">Twitter</a>
+		<a href="http://www.facebook.com/slitaz">Facebook</a>
+		<a href="http://distrowatch.com/slitaz">Distrowatch</a>
+		<a href="http://en.wikipedia.org/wiki/SliTaz">Wikipedia</a>
+		<a href="http://flattr.com/profile/slitaz">Flattr</a>
+	</p>
+</div>
+
+<div class="no">
+	<?php /* provide DokuWiki housekeeping, required in all templates */
+	tpl_indexerWebBug() ?>
+</div>
+
+</body>
+</html>
