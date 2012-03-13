6d066c7d341dc4ce94692ee4ee6aa6c98cf64205
Christophe Lincoln <pankso@slitaz.org>
2012-03-13 13:59:59 +0100

    Add pro.slitaz.org files and support

---
+++ pro/en/index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>SliTaz Pro</title>
+    <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+    <meta name="description" content="slitaz gPXE boot online your OS web-boot slitaz-cooking" />
+    <meta name="keywords" lang="en" content="slitaz, boot, pxe, web OS" />
+    <meta name="robots" content="index, follow, all" />
+    <meta name="modified" content="<?php echo (date( "Y-m-d H:i:s", getlastmod())); ?>" />
+    <meta name="author" content="Christophe Lincoln"/>
+    <link rel="shortcut icon" href="../favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="../slitaz.css" />
+</head>
+<body>
+
+<?php include("../lib/html/header.html"); ?>
+
+<!-- Block -->
+<div id="block">
+	<!-- Navigation -->
+	<div id="block_nav" style="text-align: center; padding: 10px;">
+		<img src="/images/desktop.png" />
+	</div>
+	<!-- Information/image -->
+	<div id="block_info">
+		<h4>Professional</h4>
+		<p>
+			Welcome to the <a href="http://www.slitaz.org/en/">SliTaz GNU/Linux</a>
+			professional platform. SliTaz is a fast, robust and professional
+			operating system build with open source technology.
+		</p>
+		<p>
+			We are a group of developer and partner who can provide custom
+			development and support, consulting, LiveCD flavor creation and more
+			to customers.
+		</p>
+	</div>
+</div>
+
+<!-- Languages 
+<div id="lang">
+	<a href="http://www.slitaz.org/i18n.php">
+		<img src="../images/locale.png" alt="locale.png" /></a>
+	<a href="../de/">Deutsch</a>
+	<a href="../en/">English</a>
+	<a href="../fr/">Français</a>
+</div>-->
+
+<!-- Content -->
+<div id="content">
+
+<h2>Introduction</h2>
+
+<p>
+	SliTaz is free and open source system with a large and growing
+	community, our business model is software as a service, partnership
+	and sponsoring. We can provide services all over the world
+	and in many languages such English, French and Portuguese.
+</p>
+<p>
+	This professional platform allows you to have contact with the SliTaz
+	team who will process your request and do the mediate among you 
+	and one of our official developer or sponsor. Our goal is to monitor
+	and verify the quality of service to ensure our customers a high 
+	level work.
+</p>
+
+<h2>Services</h2>
+
+<ul>
+	<li>Custom development</li>
+	<li>Consulting and audit</li>
+	<li>Custom support</li>
+	<li>Software deployment</li>
+	<li>Live system creation</li>
+</ul>
+
+<h2>SliTaz developer</h2>
+
+<p>
+	The SliTaz association is a non profit base entity who can provide
+	payed services. We can employ a freelance Slitaz developer to work
+	for you. We can also help School, ONG and social project with a
+	small budget. Please contact us for more information on this service.
+</p>
+
+<h2>Partner</h2>
+
+<p>
+	The SliTaz association will do the mediate between customer and one of
+	our partner. Official partners collaborated with the official SliTaz
+	developers and monetary support the project. Making business with our
+	official partners let you help the SliTaz project and get customized
+	services such as VOIP deployment, all in one server, Wifi Hot Spot,
+	closed development, and more.
+</p>
+
+<p>
+	If you want to get professional services from our partners of if
+	you want to become an official partner, please contact us by mail
+	or post.
+</p>
+
+<h2>Sponsor</h2>
+
+<p>
+	Do you use SliTaz in your project and want to see the distribution 
+	getting even more professional with main developers working on the
+	project full time? If so, you can sponsor the SliTaz Association 
+	which will pay a developer to work fully 100% on SliTaz.
+</p>
+
+<p>
+	If you use Slitaz for business purpose, in University or if you just
+	want to sponsor and get a logo-link-description on our main website,
+	please contact us. You can start sponsoring SliTaz from 40$ by mouth.
+	A sponsorship convention is signed for one year, the visibility of your
+	logo depend on the amount of donation received each mouth.
+</p>
+
+<p>
+	More information about sponsoring SliTaz and a list of our current
+	sponsors can be found on SliTaz website:
+	<a href="http://www.slitaz.org/en/sponsor/">Sponsor page</a>
+</p>
+
+<h2>Contact</h2>
+
+<p>
+	Want to get in touch or get more and detailled information ? Dont
+	hesitate to contact us by mail or post.
+</p>
+
+<p class="box">
+	Mail: &lt;pro at slitaz.org&gt;
+</p>
+
+<p class="box">
+	Association SliTaz<br />
+	Rue du Village 40<br />
+	1081 Montpreveyres<br />
+	Switzerland<br />
+</p>
+
+<!-- End of content -->
+</div>
+
+<?php include("../lib/html/footer.html"); ?>
+
+</body>
+</html>
---
+++ pro/index.cgi
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Content negociation for SliTaz Network
+#
+
+IFS=","
+for lang in $HTTP_ACCEPT_LANGUAGE
+do
+	lang=${lang%;*} lang=${lang# } lang=${lang%-*}
+	[ -d "$lang" ] &&  break
+done
+unset IFS
+
+echo "Location: $lang/"
+echo ""
+
+exit 0
